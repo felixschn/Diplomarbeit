d991e14c9d94e2ff139f4fd8dc98db1ab2e2f5d6
felix <s6684276@msx.tu-dresden.de>
2022-12-07 11:33:42 +0100

    made functions for random output @staticmethod added time_format string

--- Server/context_information_creation.py
+++ Server/context_information_creation.py
@@ -7,33 +7,33 @@ from dataclasses import dataclass, field
 from datetime import datetime
 
 
-def battery_information() -> int:
-    return random.randint(0, 99)
-
-
-def distance_generator() -> float:
-    return random.uniform(0.00, 999.99)
-
-
-def location_generator() -> str:
-    return str(random.uniform(1.0000000, 99.999999)) + ',' + str(random.uniform(1.0000000, 99.999999))
-
-
 @dataclass
 class ContextInformationCreation():
     # get unique identifier for each object
     identifier: int = field(default_factory=count().__next__, init=False)
     battery_state: int
 
-    #anmerkungen:
+    # anmerkungen:
     # 1)vllt bei der Distanz eine Liste nehmen, falls verschiedene Ladestationen angezeigt wreden sollen
     # 2) Säule bei Start mit einbeziehen?
     charging_station_distance: float
 
-    #bei location vllt auch ein Tuple nehmen, für die Koordinatenangabe
+    # bei location vllt auch ein Tuple nehmen, für die Koordinatenangabe
     location: str
-    elicitation_date: datetime
+    elicitation_date: str
 
+    @staticmethod
+    def battery_information() -> int:
+        return random.randint(0, 99)
 
+    @staticmethod
+    def battery_information() -> int:
+        return random.randint(0, 99)
 
-print(datetime.now().date())
+    @staticmethod
+    def distance_generator() -> float:
+        return round(random.uniform(0.00, 999.99), 2)
+
+    @staticmethod
+    def location_generator() -> str:
+        return str(random.uniform(1.0000000, 99.999999)) + ',' + str(random.uniform(1.0000000, 99.999999))
--- Server/network_class.py
+++ Server/network_class.py
@@ -1,9 +1,11 @@
 import socketserver
 import context_information_creation as cic
 from datetime import datetime
+import json
 
 HOST = "localhost"
 PORT = 9999
+time_format = '%Y-%m-%dT%H:%M:%S.%f'
 
 
 class ConnectionTCPHandler(socketserver.BaseRequestHandler):
@@ -13,22 +15,10 @@ class ConnectionTCPHandler(socketserver.BaseRequestHandler):
         print("{} wrote:".format(self.client_address[0]))
         print(self.data)
         # just send back the same data, but upper-cased
-        self.request.sendall(self.data.upper())
+        self.request.sendall(self.data)
 
 
 with socketserver.TCPServer((HOST, PORT), ConnectionTCPHandler) as server:
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
-    # server.serve_forever()
-
-    context_information_1 = cic.ContextInformationCreation(
-        cic.battery_information(), 30, "141.4378366,-25.95915986",
-        datetime.now(), datetime.now())
-
-    context_information_2 = cic.ContextInformationCreation(
-        cic.battery_information(), cic.distance_generator(), cic.location_generator(),
-        datetime.now())
-
-    print(context_information_1)
-    print(context_information_2)
-
+    server.serve_forever()
