00263812b7e990f2ee3423879020f7296050e56d
felix <s6684276@msx.tu-dresden.de>
2022-12-13 15:58:05 +0100

    edited table creation to construct a table from the transmitted dictionary created insert_query_string for dynamic data input

--- Client/context_information_database.py
+++ Client/context_information_database.py
@@ -16,7 +16,8 @@ def create_table_context_information_database(table_attributes):
     db_cursor = db_connection.cursor()
 
     db_cursor.execute("CREATE TABLE if not exists "
-                      "received_context_information(%s)" % ", ".join(table_attributes))
+                      "received_context_information(%s)" % ", ".join(
+        table_attributes.keys()))
 
     # add new column to table if table_attributes comes with additional non exising values
     for item in table_attributes:
@@ -29,11 +30,17 @@ def create_table_context_information_database(table_attributes):
 
 def insert_values_ci_db(context_information_values):
     db_cursor = db_connection.cursor()
-    insert_query = """INSERT INTO received_context_information
-                (id, battery_state, charging_station_distance, location, 
-                elicitation_date) VALUES (?,?,?,?,?)"""
+    insert_query_string = """INSERT INTO received_context_information
+                ("""
+    for key in context_information_values.keys():
+        insert_query_string = insert_query_string + key + ","
+    insert_query_string = insert_query_string[0:-1]
+    insert_query_string = insert_query_string + """) VALUES ("""
+    insert_query_string = insert_query_string + len(
+        context_information_values.keys()) * '?,'
+    insert_query_string = insert_query_string[0:-1] + ')'
 
-    db_cursor.executemany(insert_query, context_information_values)
+    db_cursor.execute(insert_query_string, list(context_information_values.values()))
     db_connection.commit()
     db_cursor.close()
 
