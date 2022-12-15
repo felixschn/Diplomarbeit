0dc485cf57dee5a6526ed3ec66804c22ab707ab2
felix <s6684276@msx.tu-dresden.de>
2022-12-15 19:05:55 +0100

    created new class ContextInformationKeystoreUpdate

---
+++ Server/context_information_keystore_update.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass, field
+from itertools import count
+
+
+@dataclass()
+class ContextInformationKeystoreUpdate:
+    identifier: int = field(default_factory=count().__next__, init=False)
+    keyname: str
+    minimum_value: float
+    maximum_value: float
+    optimum_value: float
+    weight: float
+    separator_list: list
+    update_announcement: str = field(default='context_information_keystore_update')
