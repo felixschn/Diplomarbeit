3dcb2d082cc82baa5396ac5b1d2e82d1976b531c
felix <s6684276@msx.tu-dresden.de>
2022-12-16 13:45:59 +0100

    deleted method call context_information_database added appropriate calls for each message_type to handle method
    added functionality to differentiate between various received message types to handle method
    
    added detailed explanation into exception with getframeinfo

--- Client/network_class.py
+++ Client/network_class.py
@@ -2,9 +2,10 @@ import json
 import random
 import socketserver
 from datetime import datetime, timedelta
+from inspect import getframeinfo, currentframe
 
-import Client.weights
 import Client.reasoning
+import Client.weights
 import context_information_database
 
 HOST = "localhost"
@@ -14,7 +15,53 @@ HOST = "localhost"
 PORT = random.randint(64997, 64999)
 print(PORT)
 time_format = '%Y-%m-%dT%H:%M:%S.%f'
-context_information_database.create_context_information_database()
+
+
+def process_update_messages(received_data_dict):
+    # deserialization of the received byte string back to json for creating
+    # table columns out of the dictionary keys
+    context_information_database.update_context_information_keystore(received_data_dict)
+
+
+def process_context_information_messages(received_data_dict):
+    db_table_name = 'received_context_information'
+
+    try:
+        if datetime.strptime(received_data_dict['elicitation_date'],
+                             time_format) > datetime.now() + timedelta(minutes=0):
+            print("Context elicitation error [date from received data is greater than system time]")
+            print("Context data will be ignored")
+            print("------------------------------")
+            return
+    except:
+        print("timestamp error while comparing system time with received context information")
+        return
+
+    # try:
+    #     if datetime.strptime(received_data_dict['elicitation_date'], time_format) < datetime.strptime(
+    #             context_information_database.get_latest_date_entry(db_table_name), time_format):
+    #         # TODO decide how to deal with received data which is older then the latest database entry --> save but not process or not even save?
+    #         print("Received context information is older than the latest database entry")
+    #         print("Context data will be ignored")
+    #         print("-----------------------------")
+    #         return
+    # except:
+    #     print("timestamp error while comparing the latest database entry with received context information")
+
+    received_data_dict['nation'] = 'russia'
+    options = Client.reasoning.reasoning(received_data_dict)
+
+    try:
+        weight, max_weight = Client.weights.calculate_weights(received_data_dict)
+        received_data_dict['weight'] = weight
+        best_option = Client.weights.choose_option(weight, max_weight, options)
+    except TypeError:
+        frameinfo = getframeinfo(currentframe())
+        print("""[ERROR]:weight calculation was not possible in""", frameinfo.filename, "in line:", frameinfo.lineno)
+
+    # deserialization of the received byte string back to json for creating
+    # table columns out of the dictionary keys
+    context_information_database.update_context_information(received_data_dict)
 
 
 class ConnectionTCPHandler(socketserver.StreamRequestHandler):
@@ -39,43 +86,20 @@ class ConnectionTCPHandler(socketserver.StreamRequestHandler):
             # create dict out of received data; call calculate_weights and add to dict
             try:
                 received_data_dict = json.loads(self.data)
+
+                match received_data_dict['message_type']:
+                    case 'context_information':
+                        process_context_information_messages(received_data_dict)
+                    case 'keystore_update':
+                        process_update_messages(received_data_dict)
+                    case _:
+                        print('message type is unknown')
+                        print('received data will be ignored')
+                        break
+
             except:
                 print("transformation of received data failed")
                 break
-
-            try:
-                if datetime.strptime(received_data_dict['elicitation_date'],
-                                     time_format) > datetime.now() + timedelta(minutes=0):
-                    print("Context elicitation error [date from received data is greater than system time]")
-                    print("Context data will be ignored")
-                    print("------------------------------")
-                    continue
-            except:
-                print("timestamp error while comparing system time with received context information")
-                continue
-
-            try:
-                if datetime.strptime(received_data_dict['elicitation_date'], time_format) < datetime.strptime(
-                        context_information_database.get_latest_date_entry(), time_format):
-                    # TODO decide how to deal with received data which is older then the latest database entry --> save but not process or not even save?
-                    print("Received context information is older than the latest database entry")
-                    print("Context data will be ignored")
-                    print("-----------------------------")
-                    continue
-            except:
-                print("timestamp error while comparing the latest database entry with received context information")
-
-            received_data_dict['nation'] = 'russia'
-            options = Client.reasoning.reasoning(received_data_dict)
-
-            weight, max_weight = Client.weights.calculate_weights(json.loads(self.data))
-            received_data_dict['weight'] = weight
-            best_option = Client.weights.choose_option(weight, max_weight, options)
-
-            # deserialization of the received byte string back to json for creating
-            # table columns out of the dictionary keys
-            context_information_database.create_table_context_information_database(received_data_dict)
-            context_information_database.insert_values_ci_db(received_data_dict)
 
 
 with socketserver.ThreadingTCPServer((HOST, PORT), ConnectionTCPHandler) as server:
