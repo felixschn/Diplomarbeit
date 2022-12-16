3c241e0e4403ae84d753881ac7522b2eafb0f836
felix <s6684276@msx.tu-dresden.de>
2022-12-16 13:43:04 +0100

    deleted function create_context_information_database() and moved logic to functions update_context_information_keystore and pdate_context_information
    fixed function pdate_context_information_keystore to insert keyname attribute of received message only once into database

--- Client/context_information_database.py
+++ Client/context_information_database.py
@@ -5,33 +5,6 @@ from urllib.request import pathname2url
 db_connection: sqlite3.dbapi2 = None
 
 
-# generate database to store received context information
-def create_context_information_database():
-    get_cursor()
-    add_columns()
-
-
-# retrieve entry with the latest date
-def get_latest_date_entry() -> str:
-    db_cursor = get_cursor()
-    max_date_query = "SELECT MAX(elicitation_date) From received_context_information"
-    return db_cursor.execute(max_date_query).fetchall()[0][0]
-
-
-def add_columns():
-    db_cursor = get_cursor()
-    db_cursor.execute(
-        "CREATE TABLE if not exists context_information_keystore(keyname, mini, maxi, good, weight, seperatorlist)")
-
-    # TODO abfangen, dass beim Neustart nicht erneut geschrieben wird
-    db_cursor.execute("INSERT INTO context_information_keystore(keyname,mini,maxi,good,weight,seperatorlist) "
-                      "VALUES (?,?,?,?,?,?)",
-                      ["battery_state", 0, 100, 100, 15, '[10, 20, 50, 80]'])
-    db_cursor.execute("INSERT INTO context_information_keystore(keyname,mini,maxi,good,weight,seperatorlist) "
-                      "VALUES (?,?,?,?,?,?)",
-                      ["charging_station_distance", 0, 1000, 0, 0.2, '[50, 100, 800]'])
-
-
 def get_cursor():
     global db_connection
     if db_connection is None:
@@ -40,44 +13,78 @@ def get_cursor():
         # https://stackoverflow.com/questions/12932607/how-to-check-if-a-sqlite3-database-exists-in-python
         # read, write, create database at given path
         db_uri = 'file:{}?mode=rwc'.format(pathname2url(db_name))
-        db_connection = sqlite3.connect(db_uri, uri=True)
+
+        # TODO check if check_same_thread=False may lead to any race conditions
+        # SQLite objects created in a thread can only be used in that same thread. The object was created in thread id
+        db_connection = sqlite3.connect(db_uri, uri=True, check_same_thread=False)
 
     return db_connection.cursor()
 
 
-def create_table_context_information_database(table_attributes):
+# retrieve entry with the latest date
+def get_latest_date_entry(table_name) -> str:
+    db_cursor = get_cursor()
+    validation_query = "SELECT * FROM context_information_database.tables WHERE table_name = '%s'"
+    db_cursor.execute(validation_query % table_name)
+
+    if db_cursor.fetchone()[0] == 1:
+        max_date_query = "SELECT MAX(elicitation_date) From received_context_information"
+        return db_cursor.execute(max_date_query).fetchall()[0][0]
+    else:
+        return
+
+
+def update_context_information_keystore(keystore_update_message):
+    # global db_connection
+    db_cursor = get_cursor()
+
+    # create db table if not already present
+    db_cursor.execute(
+        "CREATE TABLE if not exists context_information_keystore(keyname, mini, maxi, good, weight, seperatorlist)")
+
+    current_columns = db_cursor.execute("SELECT keyname FROM context_information_keystore").fetchall()
+    if len(current_columns) == 0:
+        pass
+    elif keystore_update_message['keyname'] in current_columns[:][0]:
+        return
+
+    # TODO abfangen, dass beim Neustart nicht erneut geschrieben wird
+    db_cursor.execute("INSERT INTO context_information_keystore(keyname,mini,maxi,good,weight,seperatorlist) "
+                      "VALUES (?,?,?,?,?,?)",
+                      list(keystore_update_message.values())[1:7])
+    db_connection.commit()
+
+
+def update_context_information(context_information_message):
+    global db_connection
     db_cursor = get_cursor()
 
     # create table and make keys from dict to new columns
     db_cursor.execute(
-        "CREATE TABLE if not exists received_context_information(%s)" % ", ".join(table_attributes.keys()))
+        "CREATE TABLE if not exists received_context_information(%s)" % ", ".join(context_information_message.keys()))
 
     # add new column to table if table_attributes comes with additional non exising values
-    for item in table_attributes:
+    for item in context_information_message:
         try:
             db_cursor.execute("ALTER TABLE received_context_information ADD COLUMN '%s'" % item)
         except:
             print("table column already exist")
-
-
-def insert_values_ci_db(context_information_values):
-    db_cursor = get_cursor()
     # create adaptive query with respect to the total amount of keys in context_information_values dict
     insert_query_string = "INSERT INTO received_context_information("
 
     # retrieve all keys from dict and write it into query; add necessary ',' to the end
-    insert_query_string += ",".join(context_information_values.keys())
+    insert_query_string += ",".join(context_information_message.keys())
 
     # append needed sql statement VALUES
     insert_query_string += ") VALUES ("
 
     # add placeholder ? for every key in context_information_values; add necessary ',' to the end
-    insert_query_string += len(context_information_values.keys()) * '?,'
+    insert_query_string += len(context_information_message.keys()) * '?,'
 
     # last ? should not have a ',' in order to have a valid query; close query string with ')'
     insert_query_string = insert_query_string[:-1] + ')'
 
     # add items to database, commit and close
-    db_cursor.execute(insert_query_string, list(context_information_values.values()))
+    db_cursor.execute(insert_query_string, list(context_information_message.values()))
     db_connection.commit()
     db_cursor.close()
