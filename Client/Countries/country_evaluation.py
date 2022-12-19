a1faf2cd135adfd15b98432a7b6ec2a789954075
felix <flxsr@tutanota.com>
2022-12-19 11:52:53 +0100

    changed path to file in order to access country_list txt removed shuffle of names in country list changed name and return value of method to allow country and id binding

--- Client/Countries/country_evaluation.py
+++ Client/Countries/country_evaluation.py
@@ -1,16 +1,16 @@
 import random
 
 
-def get_country_evaluation() -> dict:
-    with open('country_list.txt', 'r') as file:
+def get_country_code(received_contry_code) -> str:
+    #open text file with list of country names
+    with open('Countries/country_list.txt', 'r') as file:
         countries = file.read().splitlines()
 
-    random.shuffle(countries)
-
+    #create dict out of read names with increasing numbers as keys
     evaluation_dict = {}
     count = 1
     for eval in countries:
         evaluation_dict[count] = eval
         count = count + 1
 
-    return evaluation_dict
+    return evaluation_dict[received_contry_code]
