5902938b8b16112eb653dd6c49f65b6bacbfa2b9
felix <flxsr@tutanota.com>
2022-12-21 10:48:24 +0100

    wrapped the best_option calculation into try/except and added it to received_data_dict in order to store the best_option in the database
    made variables weight and max_weight global

--- Client/network_class.py
+++ Client/network_class.py
@@ -23,6 +23,7 @@ def process_update_messages(received_data_dict):
 
 
 def process_context_information_messages(received_data_dict):
+    global weight, max_weight
     db_table_name = 'received_context_information'
 
     # TODO: check timestamp in received data and compare with system time in order to neglect context information
@@ -34,7 +35,8 @@ def process_context_information_messages(received_data_dict):
             return
     except:
         frame_info = getframeinfo(currentframe())
-        print("""[ERROR]: in""", frame_info.filename, "in line:", frame_info.lineno, """while comparing system time with received context information in""")
+        print("""[ERROR]: in""", frame_info.filename, "in line:", frame_info.lineno,
+              """while comparing system time with received context information in""")
         return
 
     # try:
@@ -54,11 +56,19 @@ def process_context_information_messages(received_data_dict):
     try:
         weight, max_weight = Client.weights.calculate_weights(received_data_dict)
         received_data_dict['weight'] = weight
-        best_option = Client.weights.choose_option(weight, max_weight, options)
-        print(best_option)
     except TypeError:
         frame_info = getframeinfo(currentframe())
-        print("""[ERROR]: in""", frame_info.filename, "in line:", frame_info.lineno, """weight calculation was not possible""" )
+        print("""[ERROR]: in""", frame_info.filename, "in line:", frame_info.lineno,
+              """weight calculation was not possible""")
+
+    try:
+        best_option = Client.weights.choose_option(weight, max_weight, options)
+        print(best_option, "\n")
+        received_data_dict['best_option'] = str(best_option)
+    except:
+        frame_info = getframeinfo(currentframe())
+        print("""[ERROR]: in""", frame_info.filename, "in line:", frame_info.lineno,
+              """best_option calculation was not possible""")
 
     # deserialization of the received byte string back to json for creating
     # table columns out of the dictionary keys
@@ -67,7 +77,7 @@ def process_context_information_messages(received_data_dict):
 
 class ConnectionTCPHandler(socketserver.StreamRequestHandler):
     def handle(self) -> None:
-        print(f'Connected: {self.client_address[0]}:{self.client_address[1]}')
+        print(f'Connected: {self.client_address[0]}:{self.client_address[1]}\n')
         while True:
             try:
                 self.data = self.request.recv(1024).strip()
@@ -91,19 +101,21 @@ class ConnectionTCPHandler(socketserver.StreamRequestHandler):
                         process_update_messages(received_data_dict)
                     case _:
                         frameinfo = getframeinfo(currentframe())
-                        print("""\n[ERROR] in""", frameinfo.filename, "in line", frameinfo.lineno, """\nmessage type is unknown, received data will be ignored""")
+                        print("""\n[ERROR] in""", frameinfo.filename, "in line", frameinfo.lineno,
+                              """\nmessage type is unknown, received data will be ignored""")
                         break
 
             except json.JSONDecodeError:
                 frameinfo = getframeinfo(currentframe())
-                print("[ERROR] in""", frameinfo.filename, "in line", frameinfo.lineno, """\n'transformation of received data failed'""")
+                print("[ERROR] in""", frameinfo.filename, "in line", frameinfo.lineno,
+                      """\n'transformation of received data failed'""")
                 break
 
 
 with socketserver.ThreadingTCPServer((HOST, PORT), ConnectionTCPHandler) as server:
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
-    print("----Waiting for connection at port",PORT,"----")
+    print("----Waiting for connection at port", PORT, "----")
     server.serve_forever()
 
 # TODO:if I manually stop the run of the programm all client send context information is lost --> example: stopped process after receiving context information with id 32 --> restarted server and received information 39
