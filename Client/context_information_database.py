36a18051de8c6188d327aa6e5f031890c39ee195
felix <flxsr@tutanota.com>
2023-01-11 12:21:28 +0100

    -fixed function get_latest_date_entry in context_information_database.py; out comment timestamp validation in network_class.py -added comments -reformatted code

--- Client/context_information_database.py
+++ Client/context_information_database.py
@@ -14,39 +14,45 @@ def get_cursor():
         # read, write, create database at given path
         db_uri = 'file:{}?mode=rwc'.format(pathname2url(db_name))
 
-        # TODO check if check_same_thread=False may lead to any race conditions
-        # SQLite objects created in a thread can only be used in that same thread. The object was created in thread id
+        # TODO check if check_same_thread=False may lead to any race conditions --> error SQLite objects created in a thread can only be used in that same
+        #  thread. The object was created in thread id [...]
         db_connection = sqlite3.connect(db_uri, uri=True, check_same_thread=False)
 
     return db_connection.cursor()
 
 
-# retrieve entry with the latest date
+# retrieve the entry with the latest date
 def get_latest_date_entry(table_name) -> str:
     db_cursor = get_cursor()
-    validation_query = "SELECT * FROM context_information_database.tables WHERE table_name = '%s'"
-    db_cursor.execute(validation_query % table_name)
+    # validate if table is stored in the database; idea from: https://stackoverflow.com/questions/305378/list-of-tables-db-schema-dump-etc-using-the-python
+    # -sqlite3-api/33100538#33100538
+    validation_query = "SELECT name FROM sqlite_master WHERE type='table';"
+    db_cursor.execute(validation_query)
+    available_tables_list = [elements[0] for elements in db_cursor.fetchall()]
 
-    if db_cursor.fetchone()[0] == 1:
-        max_date_query = "SELECT MAX(elicitation_date) From received_context_information"
-        return db_cursor.execute(max_date_query).fetchall()[0][0]
-    else:
-        return
+    if table_name not in available_tables_list:
+        pass
+
+    max_date_query = "SELECT MAX(elicitation_date) From received_context_information"
+    return db_cursor.execute(max_date_query).fetchall()[0][0]
+
 
 def get_security_mechanisms_information() -> list:
     db_cursor = get_cursor()
     db_query = "SELECT * FROM  context_information_security_mechanims_information"
     return db_cursor.execute(db_query).fetchall()
 
+
 def update_context_information_security_mechanisms_information(mechanisms_information_update_message):
     db_cursor = get_cursor()
-    create_mechanisms_information_query = """CREATE TABLE if not exists 
-    context_information_security_mechanims_information(mechanism_name, modes)"""
+    create_mechanisms_information_query = """CREATE TABLE if not exists context_information_security_mechanims_information(mechanism_name, modes)"""
     db_cursor.execute(create_mechanisms_information_query)
 
     current_columns = db_cursor.execute("SELECT mechanism_name from context_information_security_mechanims_information ").fetchall()
+
     if len(current_columns) == 0:
         pass
+
     elif mechanisms_information_update_message['mechanism_name'] in [elem[0] for elem in current_columns]:
         update_query = """UPDATE context_information_security_mechanims_information SET modes = ? WHERE mechanism_name = ?"""
         query_params = list(mechanisms_information_update_message.values())[:2]
@@ -57,18 +63,19 @@ def update_context_information_security_mechanisms_information(mechanisms_inform
     insert_mechanisms_information_query = """INSERT INTO context_information_security_mechanims_information
                                     (mechanism_name, modes) 
                                     VALUES (?,?)"""
+
     query_params = list(mechanisms_information_update_message.values())[:2]
     db_cursor.execute(insert_mechanisms_information_query, query_params)
     db_connection.commit()
     return
+
 
 def update_context_information_keystore(keystore_update_message):
     # global db_connection
     db_cursor = get_cursor()
 
     # create db table if not already present
-    create_keystore_query = """CREATE TABLE if not exists context_information_keystore(keyname,minimum_value,
-    maximum_value,desirable_value,weight,separatorlist)"""
+    create_keystore_query = """CREATE TABLE if not exists context_information_keystore(keyname,minimum_value, maximum_value,desirable_value,weight,separatorlist)"""
     db_cursor.execute(create_keystore_query)
 
     # fetch all table entries to prevent keystore attribute duplicates
@@ -92,9 +99,9 @@ def update_context_information_keystore(keystore_update_message):
         db_connection.commit()
         return
 
-    insert_keystore_query = """INSERT INTO context_information_keystore
-                        (keyname,minimum_value,maximum_value,desirable_value,weight,separatorlist) 
+    insert_keystore_query = """INSERT INTO context_information_keystore(keyname,minimum_value,maximum_value,desirable_value,weight,separatorlist) 
                         VALUES (?,?,?,?,?,?)"""
+
     db_cursor.execute(insert_keystore_query, list(keystore_update_message.values())[1:7])
     db_connection.commit()
 
@@ -104,8 +111,7 @@ def update_context_information(context_information_message):
     db_cursor = get_cursor()
 
     # create table and make keys from dict to new columns
-    db_cursor.execute(
-        "CREATE TABLE if not exists received_context_information(%s)" % ", ".join(context_information_message.keys()))
+    db_cursor.execute("CREATE TABLE if not exists received_context_information(%s)" % ", ".join(context_information_message.keys()))
 
     # add new column to table if table_attributes comes with additional non-existing values
     # sqlite alter table command has no if not exists functionality therefore the command is wrapped in try/except
--- Client/network_class.py
+++ Client/network_class.py
@@ -10,16 +10,16 @@ import context_information_database
 
 HOST = "localhost"
 
-# range of possible port numbers has to be n-1 with respect to the for loop at def connection to server
-# ...in main.py, otherwise server can listen to port which is never been called from client
+# the range of possible port numbers must be n-1 with respect to the for loop in the function called connection to server in main.py, or else the server will
+# listen to a port that has never been called from the client
 PORT = random.randint(64997, 64999)
 time_format = '%Y-%m-%dT%H:%M:%S.%f'
 
 
 def process_update_messages(received_data_dict):
-    # deserialization of the received byte string back to json for creating
-    # table columns out of the dictionary keys
+    # deserialization of the received byte string back to json format in order to create table columns from dictionary keys
     context_information_database.update_context_information_keystore(received_data_dict)
+
 
 def process_security_mechanism_information(received_data_dict):
     context_information_database.update_context_information_security_mechanisms_information(received_data_dict)
@@ -31,27 +31,25 @@ def process_context_information_messages(received_data_dict):
 
     # TODO: check timestamp in received data and compare with system time in order to neglect context information
     try:
-        if datetime.strptime(received_data_dict['elicitation_date'],
-                             time_format) > datetime.now() + timedelta(minutes=0):
-            print("""[ERROR]: date from received data is greater than system time;
-                Context data will be ignored\n""")
+        if datetime.strptime(received_data_dict['elicitation_date'], time_format) > datetime.now() + timedelta(minutes=0):
+            print("""[ERROR]: date from received data is greater than system time; Context data will be ignored\n""")
             return
+
     except:
         frame_info = getframeinfo(currentframe())
-        print("""[ERROR]: in""", frame_info.filename, "in line:", frame_info.lineno,
-              """while comparing system time with received context information in""")
+        print("""[ERROR]: in""", frame_info.filename, "in line:", frame_info.lineno, """while comparing system time with received context information in""")
         return
 
-    # try:
-    #     if datetime.strptime(received_data_dict['elicitation_date'], time_format) < datetime.strptime(
-    #             context_information_database.get_latest_date_entry(db_table_name), time_format):
-    #         # TODO decide how to deal with received data which is older then the latest database entry --> save but not process or not even save?
-    #         print("Received context information is older than the latest database entry")
-    #         print("Context data will be ignored")
-    #         print("-----------------------------")
-    #         return
-    # except:
-    #     print("timestamp error while comparing the latest database entry with received context information")
+    try:
+        if datetime.strptime(received_data_dict['elicitation_date'], time_format) < datetime.strptime(
+                context_information_database.get_latest_date_entry(db_table_name), time_format):
+            # TODO decide how to deal with received data which is older then the latest database entry --> save but not process or not even save?
+            print("Received context information is older than the latest database entry")
+            print("Context data will be ignored")
+            print("-----------------------------")
+            return
+    except:
+        print("timestamp error while comparing the latest database entry with received context information")
 
     # evaluate possible set of security mechanisms with respect to data origin (country) and other params which has to be implemented
     options = Client.reasoning.reasoning(received_data_dict)
@@ -60,19 +58,19 @@ def process_context_information_messages(received_data_dict):
         weight, max_weight = Client.weights.calculate_weights(received_data_dict)
         received_data_dict['weight'] = weight
         print("calculated weight: ", weight)
+
     except TypeError:
         frame_info = getframeinfo(currentframe())
-        print("""[ERROR]: in""", frame_info.filename, "in line:", frame_info.lineno,
-              """weight calculation was not possible""")
+        print("""[ERROR]: in""", frame_info.filename, "in line:", frame_info.lineno, """weight calculation was not possible""")
 
     try:
         best_option = Client.weights.choose_option(weight, max_weight, options)
         print(best_option, "\n")
         received_data_dict['best_option'] = str(best_option)
+
     except:
         frame_info = getframeinfo(currentframe())
-        print("""[ERROR]: in""", frame_info.filename, "in line:", frame_info.lineno,
-              """best_option calculation was not possible""")
+        print("""[ERROR]: in""", frame_info.filename, "in line:", frame_info.lineno, """best_option calculation was not possible""")
 
     # deserialization of the received byte string back to json for creating
     # table columns out of the dictionary keys
@@ -85,16 +83,16 @@ class ConnectionTCPHandler(socketserver.StreamRequestHandler):
         while True:
             try:
                 self.data = self.request.recv(1024).strip()
+
             except:
                 print("----Lost connection to client----")
                 print("----Waiting for reconnection----")
                 break
 
-            print("{} wrote:".format(
-                self.client_address[0]))
+            print("{} wrote:".format(self.client_address[0]))
             print(self.data.decode('utf-8'))
 
-            # create dict out of received data and forward data to designated methods in order to process data for context evaluation
+            # create a dict out of the received data and forward the data to designated methods to process the data for context evaluation
             try:
                 received_data_dict = json.loads(self.data)
 
@@ -106,22 +104,21 @@ class ConnectionTCPHandler(socketserver.StreamRequestHandler):
                     case 'security_mechanisms_information':
                         process_security_mechanism_information(received_data_dict)
                     case _:
-                        frameinfo = getframeinfo(currentframe())
-                        print("""\n[ERROR] in""", frameinfo.filename, "in line", frameinfo.lineno,
+                        frame_info = getframeinfo(currentframe())
+                        print("""\n[ERROR] in""", frame_info.filename, "in line", frame_info.lineno,
                               """\nmessage type is unknown, received data will be ignored""")
                         break
 
             except json.JSONDecodeError:
-                frameinfo = getframeinfo(currentframe())
-                print("[ERROR] in""", frameinfo.filename, "in line", frameinfo.lineno,
+                frame_info = getframeinfo(currentframe())
+                print("[ERROR] in""", frame_info.filename, "in line", frame_info.lineno,
                       """\n'transformation of received data failed'""")
                 break
 
 
 with socketserver.ThreadingTCPServer((HOST, PORT), ConnectionTCPHandler) as server:
-    # Activate the server; this will keep running until you
-    # interrupt the program with Ctrl-C
-    print("----Waiting for connection at port", PORT, "----")
+    # Activate the server; this will keep running until an interrupt is sent to the program with Ctrl-C
+    print("---- Waiting for connection at port", PORT, "----")
     server.serve_forever()
 
 # TODO:if I manually stop the run of the programm all client send context information is lost --> example: stopped process after receiving context information with id 32 --> restarted server and received information 39
