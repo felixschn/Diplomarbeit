d8c175ce70033b0dd9930d5ade35c6b9ddbdde91
felix <flxsr@tutanota.com>
2023-01-16 10:57:34 +0100

    - fixed code style problem

--- Server/security_mechanisms_information.py
+++ Server/security_mechanisms_information.py
@@ -1,9 +1,10 @@
 from dataclasses import dataclass, field
 
+
 # class to dynamically add new information about security mechanisms who should be add to the system
 @dataclass
 class SecurityMechanismsInformation():
-    mechanism_name : str
-    modes : int
+    mechanism_name: str
+    modes: int
     mode_values: list
     message_type: str = field(default='security_mechanisms_information')
