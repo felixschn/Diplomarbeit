7f95fb31bf29a5b76442ba85b8f0ecfff3a4ef69
felix <flxsr@tutanota.com>
2023-01-25 11:28:14 +0100

    - added a check to see if the function create_all_possible_permutations retrieves any security_mechanisms_information entries from the database
    - surrounded the call filter functionality with try and except statements in case there weren't any usable filters available

--- Client/reasoning.py
+++ Client/reasoning.py
@@ -12,8 +12,6 @@ combination_cost = {}
 
 
 def apply_filters(available_security_mechanisms, context_information_dict) -> tuple:
-    available_filters = context_information_database.get_security_mechanisms_filter()
-
     try:
         # TODO store all filter in filter table to avoid looping through Filter directory; make sure that names are identical and maybe add meta information
         #  to database table for instance date of creation etc. put all files from Filter directory in a list; see:
@@ -34,8 +32,11 @@ def apply_filters(available_security_mechanisms, context_information_dict) -> tu
         imported_mod = import_module(f"Client.Filter.{formatted_file_name}")
         # get a list of all functions in the filter_file
         functions_list = getmembers(imported_mod, isfunction)
-        call_filter = getattr(imported_mod, 'execute_filter')
-        necessary_modes = call_filter(available_security_mechanisms, context_information_dict)
+        try:
+            call_filter = getattr(imported_mod, 'execute_filter')
+            necessary_modes = call_filter(available_security_mechanisms, context_information_dict)
+        except:
+            print("""some of the files in the Filter directory aren't usable filters""")
 
         for mechanism in list(available_security_mechanisms):
             # loop through the particular mechanism list
@@ -69,13 +70,19 @@ def apply_filters(available_security_mechanisms, context_information_dict) -> tu
     # return available_security_mechanisms
 
 
-# function to create all possible permutations of all security mechanisms
+# function to create all possible permutations of all security mechanisms; returns dict.keys()
 def create_all_possible_permutations(context_information_dict):
     # get all security mechanism information entries from database
     security_mechanisms_list = context_information_database.get_security_mechanisms_information()
     security_modes = {}
     security_mode_costs = {}
 
+
+    if not security_mechanisms_list:
+        frame_info = getframeinfo(currentframe())
+        print("""[ERROR]: in""", frame_info.filename, "in line:", frame_info.lineno,
+              """retireved no security mechanisms information from database""")
+        return
     # loop through all entries, create a key for the dict from the mechanism_name, and add all the modes to a list as
     # values of the dict
     for (mechanism_name, modes, mode_values) in security_mechanisms_list:
@@ -117,9 +124,7 @@ def create_all_possible_permutations(context_information_dict):
     # sort dict after values to get an order of the security mechanism combination costs
     combination_cost = dict(sorted(combination_cost.items(), key=lambda item: item[1]))
 
-    # TODO create the functionality to send a update message with different information about reducing the modes, i.e. if car is located in dangerous country
-    #  then only specific firewall combinations can be chosen; this has to be dynamic --> call it apply_filters and give information which modes has to be deployed
-
+    # TODO when applying filter, sometimes there are only costly combinations available --> so somehow we have to check if enough resources are available to choose one of the combination
     return combination_cost.keys()
 
 
