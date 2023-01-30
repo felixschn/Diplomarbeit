9827aa792a591d4ccfa984bc0b646a30447e415f
felix <flxsr@tutanota.com>
2023-01-30 09:26:32 +0100

    - renamed mode_values to mode_weights and introduced a new attribute mode_values - to aid debugging, the print statements now include the names of the various message types

--- Server/Messages/message_security_mechanism_information.py
+++ Server/Messages/message_security_mechanism_information.py
@@ -7,20 +7,21 @@ import time
 class SecurityMechanismsInformation():
     mechanism_name: str
     modes: int
+    mode_weights: list
     mode_values: list
     message_type: str = field(default="security_mechanisms_information")
 
 
 def send_security_mechanisms_information(sock):
     time_format = '%Y-%m-%dT%H:%M:%S.%f'
-    security_mechanisms_information = SecurityMechanismsInformation('checker', 5, [0, 10, 12, 13, 14])
+    security_mechanisms_information = SecurityMechanismsInformation('checker', 5, [0, 5, 10, 15, 20], [0, 2, 3, 4, 10])
 
     while True:
         if sock:
             while True:
                 sock.send(bytes(json.dumps(security_mechanisms_information.__dict__), encoding='utf-8'))
                 print(json.dumps(security_mechanisms_information.__dict__))
-                time.sleep(10)
+                time.sleep(100)
         else:
-            print("Couldn't establish socket connection")
-            print("Will try again after 10 sec ...")
+            print("Couldn't establish socket connection for security_mechanisms_information")
+            print("Will try again after 10 sec ...\n")
