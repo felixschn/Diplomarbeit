40b1982898cef532f703e46060d1e967a9129465
felix <flxsr@tutanota.com>
2023-01-31 14:27:08 +0100

    - code cleanup

--- Server/Messages/message_filter_file.py
+++ Server/Messages/message_filter_file.py
@@ -2,18 +2,17 @@ import os
 import time
 import tqdm
 
-filename = "D:\PyCharm Projects\Diplomarbeit\Server\Loading_Files\country_evaluation.py"
+filename = "D:\PyCharm Projects\Diplomarbeit\Server\Loading_Files\Filter\country_evaluation.py"
 size_of_file = os.path.getsize(f"{filename}")
 DELIMITER = "<delimiter>"
 message_type = "security_mechanisms_filter"
 BUFFER_SIZE = 4096
-count = 0
 
 
 def send_filter_file(sock):
     sock.send(f"{message_type}{DELIMITER}{filename}{DELIMITER}{size_of_file}{DELIMITER}".encode())
     show_progress = tqdm.tqdm(range(size_of_file), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
-    time.sleep(5)
+    time.sleep(2)
     with open(filename, "rb") as file:
         while True:
             read_data = file.read(BUFFER_SIZE)
