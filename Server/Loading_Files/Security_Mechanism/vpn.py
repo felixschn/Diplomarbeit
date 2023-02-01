f8f8fda0a43243936b202bc27200d45885e0a923
felix <flxsr@tutanota.com>
2023-02-01 11:14:50 +0100

    - added a security mechanism file for testing purposes

---
+++ Server/Loading_Files/Security_Mechanism/vpn.py
@@ -0,0 +1,21 @@
+import subprocess
+from inspect import getframeinfo, currentframe
+
+def vpn0():
+    try:
+        result = subprocess.run(["sudo", "wq-quick", "down", "raspi_client.conf"], capture_output=True)
+    except:
+        frame_info = getframeinfo(currentframe())
+        print("""[ERROR]: in""", frame_info.filename, "in line:", frame_info.lineno,
+              """could not turn down vpn connection""")
+        return
+
+
+def vpn1():
+    try:
+        result = subprocess.run(["sudo", "wq-quick", "up", "raspi_client.conf"], capture_output=True)
+    except:
+        frame_info = getframeinfo(currentframe())
+        print("""[ERROR]: in""", frame_info.filename, "in line:", frame_info.lineno,
+              """could not turn on vpn connection""")
+        return
