68d0e8a0dd90d21dc71a1f7cfb41e9ff28024509
felix <flxsr@tutanota.com>
2023-04-18 15:08:14 +0200

    - added simulation connection to send context_information from sumo simulation

--- Server/main.py
+++ Server/main.py
@@ -2,11 +2,11 @@ import socket
 import threading
 import time
 
-import Server.Messages.message_context_information as message_context_information
-import Server.Messages.message_keystore_information as message_keystore_information
-import Server.Messages.message_security_mechanism_information as message_security_mechanisms_information
 import Server.Messages.message_filter_file as message_filter_file
+import Server.Messages.message_keystore_information as message_keystore_information
 import Server.Messages.message_security_mechanism_file as message_security_mechanism_file
+import Server.Messages.message_security_mechanism_information as message_security_mechanisms_information
+import Server.Messages.message_weight_calculation_file as message_weight_calculation_file
 import Server.Simulation.sumo_simulation as sumo_simulation
 
 sock = None
@@ -39,18 +39,24 @@ if __name__ == '__main__':
     HOST = '127.0.0.1'
     time_format = '%Y-%m-%dT%H:%M:%S.%f'
 
-    #thread_security_mechanisms_information = threading.Thread(target=message_security_mechanisms_information.send_security_mechanisms_information,
-                                                              #args=(connection_to_server(),))
-    #thread_security_mechanisms_information.start()
+    # thread_security_mechanisms_information = threading.Thread(target=message_security_mechanisms_information.send_security_mechanisms_information,
+    # args=(connection_to_server(),))
+    # thread_security_mechanisms_information.start()
+
+
+    thread_weight_calculation_file = threading.Thread(target=message_weight_calculation_file.send_weight_calculation_file,
+                                                       args=(connection_to_server('weight_calculation_file'),))
+    thread_weight_calculation_file.start()
+    thread_weight_calculation_file.join()
 
     thread_filter_file = threading.Thread(target=message_filter_file.send_filter_file, args=(connection_to_server('filter_file'),))
     thread_filter_file.start()
     thread_filter_file.join()
 
-    thread_security_mechanism_file = threading.Thread(target=message_security_mechanism_file.send_security_mechanism_file, args=(connection_to_server('security_mechanism_file'),))
+    thread_security_mechanism_file = threading.Thread(target=message_security_mechanism_file.send_security_mechanism_file,
+                                                      args=(connection_to_server('security_mechanism_file'),))
     thread_security_mechanism_file.start()
     thread_security_mechanism_file.join()
-
 
     # thread_context_information = threading.Thread(target=message_context_information.send_context_information, args=(connection_to_server('context_information'),))
     # thread_context_information.start()
@@ -62,8 +68,10 @@ if __name__ == '__main__':
     thread_keystore_information = threading.Thread(target=message_keystore_information.send_keystore_update, args=(connection_to_server('keystore_information'),))
     thread_keystore_information.start()
 
-    thread_security_mechanisms_information = threading.Thread(target=message_security_mechanisms_information.send_security_mechanisms_information, args=(connection_to_server('security_mechanisms_information'),))
+    thread_security_mechanisms_information = threading.Thread(target=message_security_mechanisms_information.send_security_mechanisms_information,
+                                                              args=(connection_to_server('security_mechanisms_information'),))
     thread_security_mechanisms_information.start()
+
 
 
 
