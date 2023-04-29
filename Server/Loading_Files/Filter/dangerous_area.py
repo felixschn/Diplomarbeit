d7da2dde2323539c439f9f4c562480e6cd913843
felix <flxsr@tutanota.com>
2023-04-29 15:15:28 +0200

    - added dangerous_area.py file as new filter for the client to analyse if vehicle is in a dangerous area

---
+++ Server/Loading_Files/Filter/dangerous_area.py
@@ -0,0 +1,13 @@
+def execute_filter(available_security_mechanisms_list, evaluation_dict) -> list:
+    necessary_modes = ["vpn1"]
+    dangerous_area_corner_1 = (51.037275, 13.769544)
+    dangerous_area_corner_2 = (51.043342, 13.747986)
+    current_latitude, current_longitude = evaluation_dict["location"]
+
+    if dangerous_area_corner_1[0] <= current_latitude:
+        if current_latitude <= dangerous_area_corner_2[0]:
+            if dangerous_area_corner_2[1] <= current_longitude:
+                if current_longitude <= dangerous_area_corner_1[1]:
+                    return necessary_modes
+
+    return available_security_mechanisms_list
