e8e7a45cc408c9daede84608055a02db1dacd9b7
felix <flxsr@tutanota.com>
2023-04-29 17:31:39 +0200

    - added geo position calculation for simulated vehicle - added comments - reordered layout

--- Server/Simulation/sumo_simulation.py
+++ Server/Simulation/sumo_simulation.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 import traci
 
-battery_consumption_buffer = collections.deque(maxlen=20)
+battery_consumption_buffer = collections.deque(maxlen=100)
 
 
 def simulation_data(sock):
@@ -18,10 +18,9 @@ def simulation_data(sock):
         sys.exit("please declare environment variable SUMO HOME")
 
     sumoBinary = r"C:/Program Files (x86)/Eclipse/Sumo/bin/sumo-gui.exe"
-    sumoCmd = [sumoBinary, "-c", r"C:\Users\felix\Sumo\2023-04-01-09-51-10\osm.sumocfg"]
+    sumoCmd = [sumoBinary, "-c", r"C:\Users\felix\Sumo\diplomarbeit_simulation\osm.sumocfg"]
 
     traci.start(sumoCmd)
-    step = 0
     while True:
         traci.simulationStep()
 
@@ -31,13 +30,32 @@ def simulation_data(sock):
             return
 
         traci.vehicle.setColor("main_trip", (0, 255, 255))
+
+        # retrieve current position and long and lat of the simulated vehicle
+        vehicle_position_x, vehicle_position_y = traci.vehicle.getPosition("main_trip")
+        long, lat = traci.simulation.convertGeo(vehicle_position_x, vehicle_position_y)
+
+        # retrieve driven distance and remaining trip distance
+        # TODO Ask Sebastian how to deal with additional meters on the last edge
         driven_distance = traci.vehicle.getDistance("main_trip")
         trip_distance = traci.vehicle.getDrivingDistance("main_trip", "-5115636#7", traci.vehicle.getLanePosition("main_trip"))
+        trip_distance -= traci.vehicle.getLanePosition("main_trip")
+        if trip_distance < 0:
+            trip_distance = 0.0001
+
+        # retrieve name and distance to potential followers
         following_car_distance = traci.vehicle.getFollower("main_trip")
+
+        # retrieve waiting time of simulated vehicle
         accumulated_waiting_time = float(traci.vehicle.getAccumulatedWaitingTime("main_trip"))
+
+        # retrieve battery properties of simulated car
         battery_state = float(traci.vehicle.getParameter("main_trip", "device.battery.actualBatteryCapacity"))
         battery_consumption = float(traci.vehicle.getParameter("main_trip", "device.battery.energyConsumed"))
         battery_consumption_buffer.append(battery_consumption)
+        total_energy_consumption = float(traci.vehicle.getParameter("main_trip", "device.battery.totalEnergyConsumed"))
+        battery_maximum_capacity = float(traci.vehicle.getParameter("main_trip", "device.battery.maximumBatteryCapacity"))
+        battery_state_percentage = battery_state / battery_maximum_capacity
 
         # calculate average battery consumption if buffer is at least filled with 10 battery consumption values; else define average as 1
         if len(battery_consumption_buffer) < 10:
@@ -45,9 +63,8 @@ def simulation_data(sock):
         else:
             battery_consumption_average = sum(battery_consumption_buffer) / len(battery_consumption_buffer)
 
-        total_energy_consumption = float(traci.vehicle.getParameter("main_trip", "device.battery.totalEnergyConsumed"))
-        battery_maximum_capacity = float(traci.vehicle.getParameter("main_trip", "device.battery.maximumBatteryCapacity"))
-        battery_state_percentage = battery_state / battery_maximum_capacity * 100
+        if battery_consumption_average < 0:
+            battery_consumption_average = 0
 
         print("----------------------------\nDistance: ", traci.vehicle.getDistance("main_trip"))
         print("Distance_To_Target : ", traci.vehicle.getDrivingDistance("main_trip", "-5115636#7", traci.vehicle.getLanePosition("main_trip")))
@@ -58,17 +75,16 @@ def simulation_data(sock):
         print("TotalEnergy_Consumed: ", traci.vehicle.getParameter("main_trip", "device.battery.totalEnergyConsumed"))
         print("Energy_Charged: ", traci.vehicle.getParameter("main_trip", "device.battery.energyCharged"))
         print("Electricity_Consumption: ", traci.vehicle.getElectricityConsumption("main_trip"))
+        print("vehicle position: ", long, lat)
 
         # traci.vehicle.setParameter("main_trip", "device.battery.actualBatteryCapacity", 500)
         # print("Battery Capacity: ", traci.vehicle.getParameter("main_trip", "device.battery.actualBatteryCapacity"))
-
-        traci.vehicle.getRoute("main_trip")
 
         if sock:
             time_format = '%Y-%m-%dT%H:%M:%S.%f'
             context_information = {'battery_state': battery_state_percentage, 'battery_consumption': battery_consumption_average,
                                    'trip_distance': trip_distance / 1000,
-                                   'location': 125, 'elicitation_date': datetime.now().strftime(time_format), 'message_type': 'context_information'}
+                                   'location': (lat,long), 'elicitation_date': datetime.now().strftime(time_format), 'message_type': 'context_information'}
             sock.send(bytes(json.dumps(context_information), encoding='utf-8'))
             print(json.dumps(context_information))
 
