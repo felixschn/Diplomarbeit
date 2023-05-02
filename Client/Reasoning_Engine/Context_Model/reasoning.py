bcde887dedd0e0618e2e461f7677533317e3c7d7
felix <flxsr@tutanota.com>
2023-05-02 17:15:03 +0200

    - changed math.ceil to math.floor as a result of wrong combination_weight_limit calculation - changed += to extend() when adding new element to necessary_mode_list - added empty list to return if no filter files are stored in the database

--- Client/Reasoning_Engine/Context_Model/reasoning.py
+++ Client/Reasoning_Engine/Context_Model/reasoning.py
@@ -18,7 +18,7 @@ def apply_filters(context_information_dict) -> list:
         frame_info = getframeinfo(currentframe())
         print("""[ERROR]: in""", frame_info.filename, "in line:", frame_info.lineno,
               """could not retrieve filters from the database""")
-        return
+        return []
 
     # loop through the filter files
     for filter_file in filter_list:
@@ -30,7 +30,6 @@ def apply_filters(context_information_dict) -> list:
             imported_mod = import_module(f"Client.Reasoning_Engine.Filter.{formatted_file_name}")
 
         except Exception as e:
-            # TODO monitor the behavior of the program because sometimes an error occurs (maybe the function can't import the module because the filter file gets overwritten by an update message)
             print(e)
             continue
 
@@ -38,7 +37,7 @@ def apply_filters(context_information_dict) -> list:
         try:
             call_filter = getattr(imported_mod, 'execute_filter')
             # merge the returned list of the filter files to the necessary_modes_list
-            necessary_modes_list += (call_filter(available_security_mechanisms_list, context_information_dict))
+            necessary_modes_list.extend(call_filter(available_security_mechanisms_list, context_information_dict))
 
         except:
             print("""some of the files in the Filter directory aren't usable filters""")
@@ -48,16 +47,16 @@ def apply_filters(context_information_dict) -> list:
     return list(set(necessary_modes_list))
 
 
-# function to create all possible permutations of all security mechanisms; returns dict.keys()
 def calculate_best_combination(weight, max_weight, context_information_dict):
     # call filter files to retrieve necessary security mechanism modes depending on the context information
     necessary_modes = apply_filters(context_information_dict)
 
     # calculate the weight limit for combinations to choose
-    combination_weight_limit = math.ceil(weight / max_weight * context_information_database.get_max_weight_combination())
+    # TODO check if floor (abrunden) is better then ceil (aufrunden)
+    combination_weight_limit = math.floor(weight / max_weight * context_information_database.get_max_weight_combination())
     print("combination_weight_limit: ", combination_weight_limit)
 
-    # query the database for the best affordable combinations (best means highest value and lowest weight)
+    # query the database for the best affordable combinations (best means the highest value and lowest weight)
     best_affordable_combination = context_information_database.get_best_affordable_combination(combination_weight_limit, necessary_modes)
 
     return best_affordable_combination
