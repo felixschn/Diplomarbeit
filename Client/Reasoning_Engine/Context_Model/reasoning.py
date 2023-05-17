3fbb3a9dbdddc540bed78381764c7f1b8d4c6c9a
felix <flxsr@tutanota.com>
2023-05-17 16:45:30 +0200

    - altered comments - added exception error messages

--- Client/Reasoning_Engine/Context_Model/reasoning.py
+++ Client/Reasoning_Engine/Context_Model/reasoning.py
@@ -9,41 +9,44 @@ def apply_filters(context_information_dict) -> list:
     necessary_modes_list = []
 
     try:
-        # better to access information through the database instead of iterating through the filter directory, which prevents the execution of unknown (
-        # filter) files --> security vulnerabilities if attacker could add a file to the directory and their
+        # get available filter names out of database
         filter_list = context_information_database.get_security_mechanisms_filter()
 
     except:
         frame_info = getframeinfo(currentframe())
-        print("""[ERROR]: in""", frame_info.filename, "in line:", frame_info.lineno,
-              """could not retrieve filters from the database""")
+        print("[ERROR]: in", frame_info.filename, "in line:", frame_info.lineno,
+              "could not retrieve filters from the database")
         return []
 
-    # loop through the filter files
+    # loop through the filter names
     for filter_file in filter_list:
         # remove .py extension for import call
         formatted_file_name = filter_file[0].replace('.py', '')
 
-        # try to import the filter_file from Filter directory
         try:
+            # import the filter_file from Filter directory
             imported_mod = import_module(f"Client.Reasoning_Engine.Filter.{formatted_file_name}")
 
         except Exception as e:
-            print(e)
+            frame_info = getframeinfo(currentframe())
+            print("[ERROR]: in", frame_info.filename, "in line:", frame_info.lineno,
+                  "could not import filter: ", formatted_file_name)
             continue
 
-        # try to import 'execute_filter' function from filter_file
         try:
+            # import 'execute_filter' function from filter_file
             call_filter = getattr(imported_mod, 'execute_filter')
 
             # calling filter function and extend necessary_modes_list with the filter result
             necessary_modes_list.extend(call_filter(context_information_dict))
 
         except:
-            print("Some of the files in the Filter directory aren't usable filters.")
+            frame_info = getframeinfo(currentframe())
+            print("[ERROR]: in", frame_info.filename, "in line:", frame_info.lineno,
+                  "could not execute filter: ", formatted_file_name)
             continue
 
-    # return a list that contains no duplicates
+    # return necessary_modes_list that contains no duplicates
     return list(set(necessary_modes_list))
 
 
