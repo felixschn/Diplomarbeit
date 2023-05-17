4c95273452161b8449469eb2e63fcd8cada4f2ec
felix <flxsr@tutanota.com>
2023-05-17 16:47:23 +0200

    - removed percentage calculation of battery_state as it is already in percentage and TODO - altered comments

--- Server/Loading_Files/Weight_Calculations/weight_calculation_distance.py
+++ Server/Loading_Files/Weight_Calculations/weight_calculation_distance.py
@@ -3,16 +3,16 @@ import Client.Reasoning_Engine.Context_Model.Weight_Calculation.weights as weigh
 
 
 def weight_calculation_distance(evaluation_dict) -> float:
-    # charge                                                    in   %
-    # weight_calculation_distance to next charging point        in   km
-    # consumption                                               in kWh / 100km
-
+    # battery_state                                                   in   %
+    # distance                                                        in   km
+    # battery consumption                                             in kWh / 100km
 
     # define designated max_weight for special high-level context information derivation and neglect particular keystore weights
     weight_distance_calculation = 15
     weight_file.max_weight += weight_distance_calculation
 
     distance_attributes_list = ["battery_state", "battery_consumption", "trip_distance"]
+
     # define calculation specific context information attributes for high-level context information derivation
     weight_file.high_level_context_information_list.extend(distance_attributes_list)
 
@@ -25,10 +25,8 @@ def weight_calculation_distance(evaluation_dict) -> float:
         battery_state = evaluation_dict["battery_state"]
         battery_consumption = evaluation_dict["battery_consumption"]
         distance = evaluation_dict["trip_distance"]
-        capacity = 100  # in  kWh     TODO: edit param battery capacity or transmit received_message_value
         max_reserve = 3
         min_reserve = 1
-        battery_state = battery_state / 100 * capacity
         range = battery_state / battery_consumption * 100  # estimated range with current consumption
 
         reserve = range / distance
@@ -38,7 +36,7 @@ def weight_calculation_distance(evaluation_dict) -> float:
 
         if reserve < 1:
             print("reserve is critical")
-            return 0.0  # TODO or maybe even negative numbers?
+            return 0.0
 
         if reserve < 1.2:
             print("reserve is dangerous")
