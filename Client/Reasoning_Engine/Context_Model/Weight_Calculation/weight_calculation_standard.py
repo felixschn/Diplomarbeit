a5ece235f6790975ff0451bf8459e64348930aa1
felix <flxsr@tutanota.com>
2023-05-17 17:01:21 +0200

    - altered comments and spacing

--- Client/Reasoning_Engine/Context_Model/Weight_Calculation/weight_calculation_standard.py
+++ Client/Reasoning_Engine/Context_Model/Weight_Calculation/weight_calculation_standard.py
@@ -1,17 +1,25 @@
 import Client.Reasoning_Engine.Context_Model.Weight_Calculation.weights as weight_file
+
+
 def calculation(received_message_value, minimum_value, maximum_value, desirable_value, weight) -> float:
-    # only to see if the received_message_value is more on the left or on the right of the scale
+    # calculate mean to check if the received_message_value inclines to the left or on the right of the scale
     mean_value = (maximum_value - minimum_value) / 2
+
     if desirable_value > mean_value:
         if received_message_value >= desirable_value:
             return weight
+
         # normalizing data because of different value units
         normalized = (received_message_value - minimum_value) / (desirable_value - minimum_value)
+
         return normalized * weight
+
     else:
         if received_message_value < desirable_value:
             return weight
+
         normalized = (received_message_value - desirable_value) / (maximum_value - desirable_value)
+
         # when left then subtract normalized received_message_value from 1
         return (1 - normalized) * weight
 
