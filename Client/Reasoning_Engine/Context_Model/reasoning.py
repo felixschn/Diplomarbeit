ad04a83be8ba0f559600f983bcd9d2fdd6ebff74
felix <flxsr@tutanota.com>
2023-05-22 11:37:47 +0200

    - add variable for database retrieval to get the heaviest combination weight

--- Client/Reasoning_Engine/Context_Model/reasoning.py
+++ Client/Reasoning_Engine/Context_Model/reasoning.py
@@ -54,9 +54,12 @@ def calculate_best_combination(calculated_weight, max_weight, context_informatio
     # apply filters to retrieve necessary security mechanism modes
     necessary_modes = apply_filters(context_information_dict)
 
+    # retrieve the heaviest weight value from the combination from the database
+    max_weight_combination = database_connector.get_max_weight_combination()
+
     # calculate the weight limit for combinations to choose
-    combination_weight_limit = math.floor(calculated_weight / max_weight * database_connector.get_max_weight_combination())
-    print("combination_weight_limit: ", combination_weight_limit)
+    combination_weight_limit = math.floor(calculated_weight / max_weight * max_weight_combination)
+    print("combination_weight_limit: ", combination_weight_limit,"out of ", max_weight_combination)
 
     # query the database for the best affordable combinations (best means the highest value and lowest weight)
     best_affordable_combination = database_connector.get_best_affordable_combination(combination_weight_limit, necessary_modes)
