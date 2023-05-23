71a51b781568944a7d935404620c4b38f9c60979
felix <flxsr@tutanota.com>
2023-05-23 15:42:00 +0200

    - added socket connection function removed sleep

--- Server/Messages/message_keystore_information.py
+++ Server/Messages/message_keystore_information.py
@@ -1,33 +1,54 @@
 import json
-import time
+import socket
 from dataclasses import dataclass, field
-from itertools import count
 
 
 # class to dynamically update context information attributes and add new ones to the database
 @dataclass()
 class ContextInformationKeystoreUpdate:
-    identifier: int = field(default_factory=count().__next__, init=False)
     keyname: str
     minimum_value: float
     maximum_value: float
     desirable_value: float
     weight: float
-    separator_list: str
     message_type: str = field(default='keystore_update')
 
 
-def send_keystore_update(sock):
+def connection_to_server(message_name):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    port = 65000
+    host = "127.0.0.1"
+
+    # Connect to server and send data
+    try:
+        sock.connect((host, port))
+        # sock.sendall(bytes(data + "\n", "utf-8"))
+        print("Connection established")
+        return sock
+
+    except:
+        print(f"{message_name}: Couldn't connect, because wrong port or IP address was used", port)
+
+    return
+
+
+def send_keystore_update():
+    sock = connection_to_server("keystore_information")
+    print("Thread 5")
     time_format = '%Y-%m-%dT%H:%M:%S.%f'
-    context_information_keystore_update_battery_state = ContextInformationKeystoreUpdate('battery_state', 0, 100, 100,
-                                                                                         5, '[20, 40, 60, 80]')
+    context_information_keystore_update_battery_state = ContextInformationKeystoreUpdate('trip_distance', 0, 500, 0,
+                                                                                         5)
 
     while True:
         if sock:
             while True:
                 sock.send(bytes(json.dumps(context_information_keystore_update_battery_state.__dict__), encoding='utf-8'))
                 print(json.dumps(context_information_keystore_update_battery_state.__dict__))
-                time.sleep(50)
+                return
+
         else:
             print("Couldn't establish socket connection for keystore_information")
             print("Will try again after 10 sec ...\n")
+
+
+send_keystore_update()
