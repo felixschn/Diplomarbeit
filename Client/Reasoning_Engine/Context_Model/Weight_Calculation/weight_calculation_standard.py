882efa6565dc19b2baf6b0b02a2a4cfd47135231
felix <flxsr@tutanota.com>
2023-05-23 13:34:58 +0200

    - renamed function - added comments and layout changes

--- Client/Reasoning_Engine/Context_Model/Weight_Calculation/weight_calculation_standard.py
+++ Client/Reasoning_Engine/Context_Model/Weight_Calculation/weight_calculation_standard.py
@@ -1,26 +1,24 @@
 import Client.Reasoning_Engine.Context_Model.Weight_Calculation.weights as weight_file
 
 
-def calculation(received_message_value, minimum_value, maximum_value, desirable_value, weight) -> float:
-    # calculate mean to check if the received_message_value inclines to the left or on the right of the scale
+def weight_calculation_normalised(received_message_value, minimum_value, maximum_value, desirable_value, weight) -> float:
+    # calculate mean to check if the desirable_value inclines to the left or on the right of the scale
     mean_value = (maximum_value - minimum_value) / 2
 
     if desirable_value > mean_value:
+        # desirable_value strives to the maximum_value
         if received_message_value >= desirable_value:
             return weight
 
-        # normalizing data because of different value units
         normalized = (received_message_value - minimum_value) / (desirable_value - minimum_value)
-
         return normalized * weight
 
     else:
+        # desirable_ value strives to the minimum_value
         if received_message_value < desirable_value:
             return weight
 
         normalized = (received_message_value - desirable_value) / (maximum_value - desirable_value)
-
-        # when left then subtract normalized received_message_value from 1
         return (1 - normalized) * weight
 
 
@@ -30,12 +28,14 @@ def weight_standard(evaluation_dict, keystore_dict) -> float:
     for key in evaluation_dict:
         if key in weight_file.high_level_context_information_list:
             continue
+
+        # extract Keystore parameters for each key
         minimum_value = keystore_dict[key][0]
         maximum_value = keystore_dict[key][1]
         desirable_value = keystore_dict[key][2]
         context_information_weight = keystore_dict[key][3]
 
-        weight_sum += calculation(evaluation_dict[key], minimum_value, maximum_value, desirable_value, context_information_weight)
+        weight_sum += weight_calculation_normalised(evaluation_dict[key], minimum_value, maximum_value, desirable_value, context_information_weight)
         weight_file.max_weight += context_information_weight
 
     return weight_sum
