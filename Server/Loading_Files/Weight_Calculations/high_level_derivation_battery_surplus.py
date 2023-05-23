64d5a5c334e80ec8d099cf15dd953973e85f5896
felix <flxsr@tutanota.com>
2023-05-23 13:13:02 +0200

    - renamed file - removed for loop and simplified calculation - layout changes

---
+++ Server/Loading_Files/Weight_Calculations/high_level_derivation_battery_surplus.py
@@ -0,0 +1,54 @@
+from inspect import getframeinfo, currentframe
+
+import Client.Reasoning_Engine.Context_Model.Weight_Calculation.weight_calculation_standard as weight_calculation_standard
+import Client.Reasoning_Engine.Context_Model.Weight_Calculation.weights as weight_file
+
+
+def weight_calculation(evaluation_dict) -> float:
+    battery_surplus_attributes_list = ["battery_state", "battery_consumption", "trip_distance"]
+
+    # define calculation-specific context information attributes for high-level context information derivation
+    if not set(battery_surplus_attributes_list).issubset(evaluation_dict.keys()):
+        frame_info = getframeinfo(currentframe())
+        print("""[ERROR]: in""", frame_info.filename, "in line:", frame_info.lineno,
+              """missing low-level context information for calculating battery surplus""")
+        return 0
+
+    # add required low-level context information to list to ignore them in standard weight calculation
+    weight_file.high_level_context_information_list.extend(battery_surplus_attributes_list)
+
+    # battery_state                     in   %
+    # battery consumption               in kWh / 100km
+    # trip_distance                     in   km
+    battery_state = evaluation_dict["battery_state"]
+    battery_consumption = evaluation_dict["battery_consumption"]
+    trip_distance = evaluation_dict["trip_distance"]
+
+    # define Keystore parameters for high-level context information of battery surplus
+    weight_battery_surplus = 15
+    weight_file.max_weight += weight_battery_surplus
+    max_reserve = 3
+    min_reserve = 1
+
+    range = battery_state / battery_consumption * 100  # estimated range with current consumption
+    battery_surplus = range / trip_distance - 1
+
+    # battery_surplus >> 0.2     is good
+    # 0.2 > battery_surplus > 0  is dangerous
+    # 0 > battery_surplus        is critical
+    if battery_surplus < 0:
+        print("battery_surplus is critical")
+        return 0.0
+
+    if battery_surplus < 0.2:
+        print("battery_surplus is dangerous")
+        # half weight because the small battery_surplus is still critical
+        return battery_surplus * weight_battery_surplus * 0.5
+
+    if battery_surplus > max_reserve:
+        return weight_battery_surplus
+
+    print("battery_surplus is good")
+
+    # return battery_surplus with required Keystore Parameter
+    return weight_calculation_standard.calculation(battery_surplus, min_reserve, max_reserve, max_reserve, weight_battery_surplus)
