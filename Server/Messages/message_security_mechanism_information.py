e5be261a2042f0fbf430f51cbe49c3888aefc389
felix <flxsr@tutanota.com>
2023-05-23 15:41:49 +0200

    - added socket connection function removed sleep

--- Server/Messages/message_security_mechanism_information.py
+++ Server/Messages/message_security_mechanism_information.py
@@ -1,4 +1,6 @@
 import json
+import socket
+import socketserver
 import time
 from dataclasses import dataclass, field
 
@@ -13,16 +15,37 @@ class SecurityMechanismsInformation():
     message_type: str = field(default="security_mechanisms_information")
 
 
-def send_security_mechanisms_information(sock):
+def connection_to_server(message_name):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    port = 65000
+    host = "127.0.0.1"
+
+    # Connect to server and send data
+    try:
+        sock.connect((host, port))
+        # sock.sendall(bytes(data + "\n", "utf-8"))
+        print("Connection established")
+        return sock
+
+    except:
+        print(f"{message_name}: Couldn't connect, because wrong port or IP address was used", port)
+
+    return
+
+def send_security_mechanisms_information():
+    sock = connection_to_server("security_mechanisms_information")
     time_format = '%Y-%m-%dT%H:%M:%S.%f'
-    security_mechanisms_information = SecurityMechanismsInformation('vpn', 2, [0, 10], [0, 5])
+    security_mechanisms_information = SecurityMechanismsInformation('vpn', 2, [0, 15], [0, 15])
 
     while True:
         if sock:
             while True:
                 sock.send(bytes(json.dumps(security_mechanisms_information.__dict__), encoding='utf-8'))
                 print(json.dumps(security_mechanisms_information.__dict__))
-                time.sleep(100)
+                return
+
         else:
             print("Couldn't establish socket connection for security_mechanisms_information")
             print("Will try again after 10 sec ...\n")
+
+send_security_mechanisms_information()
