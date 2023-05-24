7137ea11c89bc846cf5d0c66100e0a1cf8d56f2a
felix <flxsr@tutanota.com>
2023-05-24 09:06:25 +0200

    - changed static to dynamic path declaration - changed old variable names weight to costs for security mechanism related functionalities - changed variable name received_data_dict to received_information

--- Client/Application_Area/network_class.py
+++ Client/Application_Area/network_class.py
@@ -4,11 +4,12 @@ import socketserver
 from datetime import datetime, timedelta
 from importlib import reload, import_module
 from inspect import getframeinfo, currentframe
+from pathlib import Path
 
 import tqdm
 
 import Client.Application_Area.Security_Mechanisms.set_security_mechanisms
-import Client.Reasoning_Engine.Context_Model.Weight_Calculation.asset_evaluation
+import Client.Reasoning_Engine.Context_Model.Rule_Set.asset_evaluation
 import Client.Reasoning_Engine.Context_Model.reasoning
 from Client.Data_Engine import database_connector
 
@@ -41,13 +42,14 @@ def process_incoming_message(received_data, connection_handler, module_storing_p
 
     # store the received data in variables
     _, filename, size_of_file, file_content = received_data.split(DELIMITER)
-
-    filename = os.path.basename(filename)
     size_of_file = int(size_of_file)
     show_progress = tqdm.tqdm(range(size_of_file), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
 
-    # write the received data to a file in the 'Weight_Calculation' directory
-    with open(module_storing_path + filename, "wb") as received_file:
+    # write the received data to a file in the 'Rule_Set' directory
+    path_to_project = Path(__file__).parents[2]
+    path_to_file = path_to_project.joinpath(module_storing_path + filename)
+
+    with open(path_to_file, "wb") as received_file:
         while True:
             read_data = connection_handler.request.recv(BUFFER_SIZE)
 
@@ -68,8 +70,8 @@ def process_incoming_message(received_data, connection_handler, module_storing_p
 
 
 def process_message_high_level_derivation_file(received_data, connection_handler):
-    modul_storing_path = f"D:\PyCharm Projects\Diplomarbeit\Client\Reasoning_Engine\Context_Model\Weight_Calculation\\"
-    module_path = "Client.Reasoning_Engine.Context_Model.Weight_Calculation."
+    modul_storing_path = f"Client\Reasoning_Engine\Context_Model\Rule_Set\\"
+    module_path = "Client.Reasoning_Engine.Context_Model.Rule_Set."
     filename = process_incoming_message(received_data, connection_handler, modul_storing_path, module_path)
 
     # update the database with a filter name from the added filter file
@@ -77,13 +79,13 @@ def process_message_high_level_derivation_file(received_data, connection_handler
 
 
 def process_message_security_mechanism_file(received_data, connection_handler):
-    modul_storing_path = f"D:\PyCharm Projects\Diplomarbeit\Client\Application_Area\Security_Mechanisms\\"
+    modul_storing_path = f"Client\Application_Area\Security_Mechanisms\\"
     module_path = "Client.Application_Area.Security_Mechanisms."
     filename = process_incoming_message(received_data, connection_handler, modul_storing_path, module_path)
 
 
 def process_message_filter_file(received_data, connection_handler):
-    modul_storing_path = f"D:\PyCharm Projects\Diplomarbeit\Client\Reasoning_Engine\Filter\\"
+    modul_storing_path = f"Client\Reasoning_Engine\Filter\\"
     module_path = "Client.Reasoning_Engine.Filter."
     filename = process_incoming_message(received_data, connection_handler, modul_storing_path, module_path)
 
@@ -97,23 +99,23 @@ def process_message_keystore_information(received_data_dict):
 
 
 def process_message_security_mechanisms_information(received_data_dict):
-    # check if the number of mode_weights and modes is not equal
-    if received_data_dict['modes'] != len(received_data_dict['mode_weights']) or received_data_dict['modes'] != len(received_data_dict['mode_values']):
+    # check if the number of mode_costs and modes is not equal
+    if received_data_dict['modes'] != len(received_data_dict['mode_costs']) or received_data_dict['modes'] != len(received_data_dict['mode_values']):
         frame_info = getframeinfo(currentframe())
         print("""[ERROR]: in""", frame_info.filename, "in line:", frame_info.lineno,
-              """update message for security_mechanism_information: the number of modes and mode weights or mode values are not equal""")
+              """update message for security_mechanism_information: the number of modes and mode costs or mode values are not equal""")
         return
 
     # write retrieved information to the database
     database_connector.update_security_mechanisms_information(received_data_dict)
 
-    # call function to create new combinations, weights and values for the updated security mechanism information
+    # call function to create new combinations, costs and values for the updated security mechanism information
     database_connector.create_security_mechanism_combinations()
 
 
-def process_message_context_information(received_data_dict):
+def process_message_context_information(received_information):
     try:
-        if datetime.strptime(received_data_dict['elicitation_date'], time_format) > datetime.now() + timedelta(minutes=0):
+        if datetime.strptime(received_information['elicitation_date'], time_format) > datetime.now() + timedelta(minutes=0):
             print("""[ERROR]: date from received data is greater than system time; Context data will be ignored\n""")
             return
 
@@ -123,7 +125,7 @@ def process_message_context_information(received_data_dict):
         return
 
     # try:
-    #     if datetime.strptime(received_data_dict['elicitation_date'], time_format) < datetime.strptime(
+    #     if datetime.strptime(received_information['elicitation_date'], time_format) < datetime.strptime(
     #             database_connector.get_latest_date_entry(db_table_name), time_format):
     #         # TODO decide how to deal with received data which is older then the latest database entry --> save but not process or not even save?
     #         print("Received context information is older than the latest database entry")
@@ -134,8 +136,8 @@ def process_message_context_information(received_data_dict):
     #     print("timestamp error while comparing the latest database entry with received context information")
 
     try:
-        calculated_asset, sum_of_max_asset = Client.Reasoning_Engine.Context_Model.Weight_Calculation.asset_evaluation.asset_evaluation(received_data_dict)
-        received_data_dict['asset'] = calculated_asset
+        calculated_asset, sum_of_max_asset = Client.Reasoning_Engine.Context_Model.Rule_Set.asset_evaluation.asset_evaluation(received_information)
+        received_information['asset'] = calculated_asset
         print("calculated asset: ", calculated_asset)
 
     except:
@@ -145,9 +147,9 @@ def process_message_context_information(received_data_dict):
         return
 
     try:
-        best_option = Client.Reasoning_Engine.Context_Model.reasoning.calculate_best_combination(calculated_asset, sum_of_max_asset, received_data_dict)
+        best_option = Client.Reasoning_Engine.Context_Model.reasoning.calculate_best_combination(calculated_asset, sum_of_max_asset, received_information)
         print(best_option, "\n")
-        received_data_dict['best_option'] = str(best_option)
+        received_information['best_option'] = str(best_option)
 
     except:
         frame_info = getframeinfo(currentframe())
@@ -156,9 +158,9 @@ def process_message_context_information(received_data_dict):
         return
 
     # convert location tuple with long and lat to string in order to save it in the database
-    received_data_dict["location"] = str(received_data_dict["location"])
+    received_information["location"] = str(received_information["location"])
     # save context information with best option evaluation to the database
-    database_connector.update_context_information(received_data_dict)
+    database_connector.update_context_information(received_information)
 
     # check if the best_option tuple is not empty
     # if best_option:
