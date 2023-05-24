5a5fe6d508e70b98e0e8d3b9fb7c91ff14f2bee8
felix <flxsr@tutanota.com>
2023-05-24 09:09:24 +0200

    - changed function name weight_evaluation to asset_evaluation as well as variable names and database queries

--- Client/Reasoning_Engine/Context_Model/Rule_Set/asset_evaluation.py
+++ Client/Reasoning_Engine/Context_Model/Rule_Set/asset_evaluation.py
@@ -3,16 +3,16 @@ from importlib import import_module
 from inspect import getframeinfo, currentframe
 
 import Client.Data_Engine.database_connector as database_connector
-from Client.Reasoning_Engine.Context_Model.Weight_Calculation.weight_calculation_standard import weight_calculation_standard
+from Client.Reasoning_Engine.Context_Model.Rule_Set.asset_calculation_standard import asset_calculation_standard
 
-max_sum_of_weight = 0
+max_sum_of_asset = 0
 high_level_context_information_list = []
 
 
-def weight_evaluation(received_context_information) -> tuple[float, float]:
-    global max_sum_of_weight, high_level_context_information_list
-    calculated_weight = 0
-    max_sum_of_weight = 0
+def asset_evaluation(received_context_information) -> tuple[float, float]:
+    global max_sum_of_asset, high_level_context_information_list
+    calculated_asset = 0
+    max_sum_of_asset = 0
     # create database cursor
     db_cursor = database_connector.get_cursor()
 
@@ -41,33 +41,33 @@ def weight_evaluation(received_context_information) -> tuple[float, float]:
             evaluation_dict[key] = received_context_information[key]
 
     try:
-        derivation_files_query = "SELECT * FROM weight_calculation_files"
-        weight_files_list = db_cursor.execute(derivation_files_query).fetchall()
+        derivation_files_query = "SELECT * FROM high_level_derivation_files"
+        high_level_derivation_list = db_cursor.execute(derivation_files_query).fetchall()
 
     except sqlite3.OperationalError:
         frame_info = getframeinfo(currentframe())
-        print("\n[ERROR]: in", frame_info.filename, "in line:", frame_info.lineno, "Database does not contain weight calculation files;\nStandard weight "
+        print("\n[ERROR]: in", frame_info.filename, "in line:", frame_info.lineno, "Database does not contain high-level derivation files;\nStandard weight "
                                                                                    "calculation will be used;\n")
-        calculated_weight += weight_calculation_standard(evaluation_dict, keystore_dict)
-        return calculated_weight, max_sum_of_weight
+        calculated_asset += asset_calculation_standard(evaluation_dict, keystore_dict)
+        return calculated_asset, max_sum_of_asset
 
-    for weight_calculation_file in weight_files_list:
-        formatted_file_name = weight_calculation_file[0].replace('.py', '')
+    for high_level_derivation_file in high_level_derivation_list:
+        formatted_file_name = high_level_derivation_file[0].replace('.py', '')
 
         try:
             # import received source code as python module
-            imported_mod = import_module(f"Client.Reasoning_Engine.Context_Model.Weight_Calculation.{formatted_file_name}")
+            imported_mod = import_module(f"Client.Reasoning_Engine.Context_Model.Rule_Set.{formatted_file_name}")
 
         except:
             frame_info = getframeinfo(currentframe())
             print("[ERROR]: in", frame_info.filename, "in line:", frame_info.lineno,
-                  "could not import weight calculation file: ", formatted_file_name)
+                  "could not import high-level derivation file: ", formatted_file_name)
             continue
 
         try:
             # loading and calling high-level_derivation function to calculate weight
             call_high_level_derivation = getattr(imported_mod, 'high_level_derivation')
-            calculated_weight += (call_high_level_derivation(evaluation_dict))
+            calculated_asset += (call_high_level_derivation(evaluation_dict))
 
         except:
             frame_info = getframeinfo(currentframe())
@@ -75,6 +75,6 @@ def weight_evaluation(received_context_information) -> tuple[float, float]:
                   "could not call high_level_derivation: ", formatted_file_name)
             continue
 
-    calculated_weight += weight_calculation_standard(evaluation_dict, keystore_dict)
+    calculated_asset += asset_calculation_standard(evaluation_dict, keystore_dict)
 
-    return calculated_weight, max_sum_of_weight
+    return calculated_asset, max_sum_of_asset
