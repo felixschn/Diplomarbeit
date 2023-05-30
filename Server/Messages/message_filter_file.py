fd92afa463399983cc5c23d7f4c86caa9bc8a73f
felix <flxsr@tutanota.com>
2023-05-30 10:09:01 +0200

    - removed local connection_to_server function and utilised function in main.py - removed print statements - altered path declaration by escaping \ with another \ - added comments

--- Server/Messages/message_filter_file.py
+++ Server/Messages/message_filter_file.py
@@ -3,11 +3,12 @@ import time
 from pathlib import Path
 
 import tqdm
-import socket
 
+import Server.main as main
 
+# create dynamic path declarations and file information
 path_to_project = Path(__file__).parents[2]
-path_to_file = path_to_project.joinpath("Server\Loading_Files\Filter\\follower.py")
+path_to_file = path_to_project.joinpath("Server\\Loading_Files\\Filter\\dangerous_area.py")
 size_of_file = os.path.getsize(f"{path_to_file}")
 filename = path_to_file.name
 DELIMITER = "<delimiter>"
@@ -15,29 +16,18 @@ message_type = "security_mechanisms_filter"
 BUFFER_SIZE = 4096
 
 
-def connection_to_server(message_name):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    port = 65000
-    host = "127.0.0.1"
-
-    # Connect to server and send data
-    try:
-        sock.connect((host, port))
-        # sock.sendall(bytes(data + "\n", "utf-8"))
-        print("Connection established")
-        return sock
-
-    except:
-        print(f"{message_name}: Couldn't connect, because wrong port or IP address was used", port)
-
-    return
-
 def send_filter_file():
-    sock = connection_to_server("filter_file")
-    print("Thread 3")
+    # establish a socket connection by calling the function in main.py
+    sock = main.connection_to_server("filter_file")
+
+    # send a message identifier string that contains the message type, filename, and size of the file
     sock.send(f"{message_type}{DELIMITER}{filename}{DELIMITER}{size_of_file}{DELIMITER}".encode())
+
+    # create a graphical bar that will show the progress of the transmission
     show_progress = tqdm.tqdm(range(size_of_file), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
-    time.sleep(2)
+    time.sleep(1)
+
+    # reading the security mechanism file from the Sever and sending it to the Client
     with open(path_to_file, "rb") as file:
         while True:
             read_data = file.read(BUFFER_SIZE)
@@ -47,5 +37,5 @@ def send_filter_file():
             show_progress.update(len(read_data))
     sock.close()
 
-send_filter_file()
 
+send_filter_file()
