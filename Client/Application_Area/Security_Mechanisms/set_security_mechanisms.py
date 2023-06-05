c9588008b92d84aac05338ca9b54494fbafe1358
felix <flxsr@tutanota.com>
2023-06-05 19:14:57 +0200

    - added execution functionality for security mechanisms

--- Client/Application_Area/Security_Mechanisms/set_security_mechanisms.py
+++ Client/Application_Area/Security_Mechanisms/set_security_mechanisms.py
@@ -1,28 +1,23 @@
+import re
 from importlib import import_module
 from inspect import getframeinfo, currentframe
-from os import listdir
-from os.path import isfile, join
+
+import Client.Data_Engine.database_connector as database_connector
 
 
 def set_security_mechanisms(best_option):
     try:
-        security_mechanisms_path = ""
-        list_security_mechanism_files = [file for file in listdir(security_mechanisms_path) if isfile(join(security_mechanisms_path, file))]
-
+        security_mechanisms_list = database_connector.get_security_mechanism_names()
     except FileNotFoundError:
         frame_info = getframeinfo(currentframe())
         print("""[ERROR]: in""", frame_info.filename, "in line:", frame_info.lineno,
-              """could not find security mechanism files in Security_Mechanisms directory""")
+              "no security mechanism names found in database")
         return
 
-    for security_mechanisms_file in list_security_mechanism_files:
-        # remove '.py' extension for import call
-        formatted_file_name = security_mechanisms_file.replace('.py', '')
-        # import file from Security_Mechanisms directory
-        imported_mod = import_module(f"Client.Security_Mechanisms.{formatted_file_name}")
-
-        if any(formatted_file_name in item for item in best_option):
-            for mode in best_option:
-                if mode.startswith(formatted_file_name):
-                    call_filter = getattr(imported_mod, mode)
-                    call_filter()
+    # extract security mechanism modes from best options and execute them
+    for security_mechanism in best_option.split(","):
+        mechanism_name = "".join((re.findall(r"[a-zA-Z]+", security_mechanism)))
+        mode_number = int("".join((re.findall(r"\d+", security_mechanism))))
+        imported_mod = import_module(f"Client.Application_Area.Security_Mechanisms.{mechanism_name}")
+        security_mechanism_entry_point = getattr(imported_mod, mechanism_name)
+        security_mechanism_entry_point(mode_number)
