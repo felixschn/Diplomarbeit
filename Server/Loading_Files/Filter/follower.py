9c5aa2a97faccccea91507546cc6e868bdff2e65
felix <flxsr@tutanota.com>
2023-06-05 19:17:18 +0200

    - simplified if statements - added comments

--- Server/Loading_Files/Filter/follower.py
+++
@@ -1,9 +0,0 @@
-def execute_filter(evaluation_dict) -> list:
-    necessary_modes = ["fw3"]
-
-    if evaluation_dict["follower"] > 0:
-        if evaluation_dict["follower"] < 50:
-            print(f"Follower Filter activated: ", necessary_modes)
-            return necessary_modes
-
-    return []
