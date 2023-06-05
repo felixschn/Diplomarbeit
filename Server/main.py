3b9d66c81460948a683c7e6841f12fb91c8ca460
felix <flxsr@tutanota.com>
2023-06-05 19:16:03 +0200

    - altered variables and comments

--- Server/main.py
+++ Server/main.py
@@ -1,25 +1,26 @@
 import socket
 
-import Server.Simulation.sumo_simulation as sumo_simulation
+import Server.Simulation.run_sumo as run_sumo
+
+HOST = "127.0.0.1"
+PORT = 65000
 
 
 def connection_to_server(message_name):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    port = 65000
-    host = '127.0.0.1'
 
-    # Connect to Client
+    # connect to Client
     try:
-        sock.connect((host, port))
-        print('Connection established')
+        sock.connect((HOST, PORT))
+        print('Connection established:')
         return sock
 
     except:
-        print(f"{message_name}: Couldn't connect, because wrong port or IP address was used", port)
+        print(f"{message_name}: Couldn't connect, because wrong port or IP address was used")
 
     return
 
 
 if __name__ == '__main__':
-    # execute SUMO simulation
-    sumo_simulation.simulation_data(connection_to_server("context_information"))
+    sock = connection_to_server("context_information")
+    run_sumo.simulation_data(sock)
