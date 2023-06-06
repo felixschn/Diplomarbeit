052ab00132e6846c4db298f0af66c3974dd8621d
felix <flxsr@tutanota.com>
2023-06-06 23:21:48 +0200

    - increase sleep to 2 sec

--- Server/Messages/send_security_mechanism_file.py
+++ Server/Messages/send_security_mechanism_file.py
@@ -26,7 +26,7 @@ def send_security_mechanism_file():
 
     # create a graphical bar that will show the progress of the transmission; optional delay for better graphical representation
     show_progress = tqdm.tqdm(range(size_of_file), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
-    time.sleep(1)
+    time.sleep(2)
 
     # reading the security mechanism file from the Sever and sending it to the Client
     with open(path_to_file, "rb") as file:
