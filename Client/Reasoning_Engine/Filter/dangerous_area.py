ec87fc0b819e6c5fcc0cf49075df41c0c789c65b
felix <flxsr@tutanota.com>
2023-06-07 15:23:45 +0200

    - adding dangerous_area.py to git to include Filter directory

---
+++ Client/Reasoning_Engine/Filter/dangerous_area.py
@@ -0,0 +1,14 @@
+def execute_filter(evaluation_dict) -> list:
+    necessary_modes = ["vpn1"]
+    # defining coordinates for dangerous area in sumo map
+    dangerous_area_corner_1 = (51.037275, 13.769544)
+    dangerous_area_corner_2 = (51.043342, 13.747986)
+    current_latitude, current_longitude = evaluation_dict["location"]
+
+    # check if vehicle is in dangerous area
+    if dangerous_area_corner_1[0] <= current_latitude <= dangerous_area_corner_2[0]:
+        if dangerous_area_corner_2[1] <= current_longitude <= dangerous_area_corner_1[1]:
+            print(f"Dangerous Area Filter activated: ", necessary_modes)
+            return necessary_modes
+
+    return []
