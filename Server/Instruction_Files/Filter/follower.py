73aefe5c2f441a691139ab223f88b00d3fe61454
felix <flxsr@tutanota.com>
2023-06-08 08:59:16 +0200

    - added comment

--- Server/Instruction_Files/Filter/follower.py
+++ Server/Instruction_Files/Filter/follower.py
@@ -1,6 +1,7 @@
 def execute_filter(received_context_information) -> list:
     necessary_modes = ["fw3"]
 
+    # check whether the follower is within a distance of 50 metres
     if 0 < received_context_information["follower"] < 50:
         print(f"Follower Filter activated: ".ljust(33), necessary_modes)
         return necessary_modes
